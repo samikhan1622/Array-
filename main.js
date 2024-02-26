ac07b5b5c7238cdc10287286cbbe6271c00c9c09
Sami Ullah <153662652+samikhan1622@users.noreply.github.com>
2024-02-26 16:52:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+//             (Array)
+//       example 01
+// let fruit1 = "apple";
+// let fruit1 = "orange";
+// let fruit1 = "banana";
+// let fruit1 = "pineapple";
+//      example 02
+// let fruits: string[] = ["apple", "orange", "banana", "pineapple"];
+// let subjects: string[] = ["math", "science", "english", "history"];
+// let ages: number[] = [1, 2, 3, 4, 5, 6];
+// let fruit: string[] = ["apple", "orange", "banana", "pineapple"];
+// console.log(fruit[0], fruit[2]);
+// let menu: string[] = ["apple", "orange", "banana", "pineapple"];
+// menu.pop();
+// menu.push("chicken tikka")
+// menu.pop();
+// let removeitem = menu.shift()
+// console.log(removeitem)
+// let removeitem = menu.unshift()
+// console.log(removeitem);
+// let menues: string[] = ["apple", "orange", "banana", "pineapple"];
+// // let sliceitem = menues.slice(1);
+// // console.log(sliceitem);
+// let sliceitem = menues.splice(0);
+// console.log(sliceitem);
